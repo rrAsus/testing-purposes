0273326e9cbc90e55b1e4e40db2e666e080933e5
N/A <102998736+rrAsus@users.noreply.github.com>
2023-08-17 18:13:32 +0200

    Create Changelog text.lua

---
+++ Changelog text.lua
@@ -0,0 +1,10 @@
+return {
+    Version = "", -- Change this to the version of your script
+    News = { 
+        '> Changed To A Better UI',
+    },
+    Title = 'The Intruders',
+    MainText = '',
+    DelayToDestroy = 60,
+    From = 'FakeBadger (Script Owner)', 
+}
