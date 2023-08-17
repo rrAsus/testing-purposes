e12600f5a1df35f9280b9204b5e3d8ec6d0515ff
N/A <102998736+rrAsus@users.noreply.github.com>
2023-08-18 01:56:39 +0200

    Update changelogui.lua

--- changelogui.lua
+++ changelogui.lua
@@ -1,14 +1,3 @@
---[[
- .____                  ________ ___.    _____                           __                
- |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
- |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
- |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
- |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
-         \/          \/         \/    \/                \/     \/     \/                   
-          \_Welcome to LuaObfuscator.com   (Alpha 0.9.14) ~  Much Love, Ferib 
-
-]]--
-
 local function InitChangeLog(Settings)
 	assert(typeof(Settings) == "table", "Invalid argument #1 (table expected)");
 	assert(typeof(Settings.News) == "table", "Invalid format to 'Settings.News' (table expected)");
