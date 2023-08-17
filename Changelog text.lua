8ff0d94d2a8bcec7ef713204dd68a84b4f0ad1a3
N/A <102998736+rrAsus@users.noreply.github.com>
2023-08-18 01:56:27 +0200

    Delete Changelog text.lua

--- Changelog text.lua
+++
@@ -1,9 +0,0 @@
-return {
-    News = { 
-        '[+] Changed To A Better UI',
-    },
-    Title = 'Javolo Hub Universal',
-    MainText = '',
-    DelayToDestroy = 60,
-    From = 'FakeBadger (Script Owner)', 
-}
